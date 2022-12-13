e596b3e33bc300e0b1782d3089e78e40e9a41ad9
blue <1355589128@qq.com>
2022-12-13 16:46:26 +0800

    init

--- src/pages/user/Login/index.tsx
+++ src/pages/user/Login/index.tsx
@@ -72,7 +72,7 @@ const Login: React.FC = () => {
         history.push(redirect || '/');
         return;
       }
-      console.log(msg);
+
       // 如果失败去设置用户错误信息
       setUserLoginState(msg);
     } catch (error) {
